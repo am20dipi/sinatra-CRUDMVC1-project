ad0211e6aec7d10c2a6f1d0c361bab0ed68e3146
am20dipi <am20dipi@alum.siena.edu>
2020-10-27 12:56:55 -0400

    nailpolishescontroller

--- app/controllers/nailpolishes_controller.rb
+++ app/controllers/nailpolishes_controller.rb
@@ -22,4 +22,33 @@ class NailPolishesController < ApplicationController
       erb :new
     end
 
+    post '/nailpolishes' do 
+        user = User.find_by(id: params[:user_id])
+        nailpolish = user.nailpolish.build(params) # .build creates but does NOT SAVE
+        if nailpolish.save # .save returns true or false
+            redirect "/nailpolishes/#{nailpolish.id}"
+        else
+            redirect '/nailpolish/new'
+        end
+    end
+    
+    get '/nailpolishes/:id/edit' do 
+        @nailpolish = NailPolish.find_by(id: params[:id]) 
+        erb :edit 
+    end
+    
+    patch '/nailpolishes/:id' do 
+        binding.pry
+    end
+    
+    get '/nailpolishes/:id' do # :id adds the key to the params hash
+        @nailpolish = NailPolish.find_by(id: params[:id])
+    
+        if @nailpolish 
+            erb :show
+        else
+            redirect '/nailpolish'
+        end
+    end
+
 end
