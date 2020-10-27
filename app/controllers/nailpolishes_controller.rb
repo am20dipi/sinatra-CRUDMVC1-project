126ee3949b46af9768182ffae18c48368e368fd9
am20dipi <am20dipi@alum.siena.edu>
2020-10-27 12:50:51 -0400

    nailpolishcontroler

--- app/controllers/nailpolishes_controller.rb
+++ app/controllers/nailpolishes_controller.rb
@@ -1,4 +1,25 @@
 class NailPolishesController < ApplicationController
-
+    
+    get '/' do
+    end
+  
+    get '/nailpolishes' do 
+      @nailpolishes = NailPolish.all 
+  
+      erb :index 
+    end
+  
+  
+    get '/nailpolishes/:id' do 
+      @nailpolish = NailPolish.new(params[:id])
+  
+      erb :show 
+    end
+  
+  
+    get '/nailpolishes/new' do 
+      @users = User.all
+      erb :new
+    end
 
 end
