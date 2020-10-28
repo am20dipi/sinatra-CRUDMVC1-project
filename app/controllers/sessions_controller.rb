3297c0d48e97d8ca045c305e10ad58ff27587878
am20dipi <am20dipi@alum.siena.edu>
2020-10-28 13:39:06 -0400

    sessionslogout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,9 +10,13 @@ class SessionsController < ApplicationController
             session[:user_id] = user.id #this is the line of code that shows that a user logged in!! 
             redirect '/nailpolishes' #to correctly log in the params, we put it in the session. we are persisting it to the next request
         else
-            redirect ''
+            redirect '/login'
         end
-        
+    end
+
+    get '/logout' do 
+        session.clear
+        redirect '/login'
     end
 
 end
