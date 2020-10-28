4b58844d7d2663f80c0aff964b982f0bfe0950cd
am20dipi <am20dipi@alum.siena.edu>
2020-10-28 13:36:20 -0400

    sessionscontroller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,13 @@ class SessionsController < ApplicationController
 
     post '/login' do 
         user = User.find_by(email: params[:email])
-        session[:email] = params[:email] #to correctly log in the params, we put it in the session. we are persisting it to the next request
-        redirect '/nailpolishes'
+        if user && user.authenticate(params[:password]) #authenticate method is bcrypt working with AR 
+            session[:user_id] = user.id #this is the line of code that shows that a user logged in!! 
+            redirect '/nailpolishes' #to correctly log in the params, we put it in the session. we are persisting it to the next request
+        else
+            redirect ''
+        end
+        
     end
 
 end
